d80ed162615e98c30a8a9e199659554e8cf58263
Nick Dandakis <nd584@york.ac.uk>
2014-05-21 21:56:25 +0100

    Added some basic commenting, cbf to add more at the moment.

--- src/CompoundFinder.java
+++ src/CompoundFinder.java
@@ -4,74 +4,98 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CompoundFinder {
-	static TrieNode trie;
-	static ArrayList<String> words;
+	static TrieNode trie; 				// the glorious trie
+	static ArrayList<String> words; 	// the words to find compounds for
+	static int wordLength;				// the specific word length query words need to be
+	static File dictionary;				// the dictionary file
 	
 	public static void main(String[] args) {
+		userInput();
 		populateTrie();
 		findCompounds();
 	}
 	
-	public static void populateTrie(){
-		trie = new TrieNode(null);
-		words = new ArrayList<String>();
-		Scanner sc = null;
-		try {
-			sc = new Scanner(new File("/usr/share/dict/words")); // TODO UI 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace(); // TODO make more meaningful
+	public static void userInput(){
+		Scanner systemIn = new Scanner(System.in);
+		
+		System.out.println("## Mint Digital coding test - Nick Dandakis ##");
+		System.out.println("Processes dictionary, looking for words that are composed of two concatenated smaller words.\n");
+		
+		// Grab word length and do some validation for minimum word length
+		System.out.print("Word length: ");
+		wordLength = Integer.valueOf(systemIn.next());
+		while(wordLength < 4){
+			System.out.println("Error: Minimum word length is 4!");
+			System.out.print("Word length: ");
+			wordLength = Integer.valueOf(systemIn.next());
+		}		
+		
+		// Grab dictionary location and do some validation for existing file
+		// I could've added a default location of /usr/share/dict/words, but I'll leave that as a TODO
+		String dictionaryLocation;
+		System.out.print("Dictionary location: ");
+		dictionaryLocation = systemIn.next();
+		dictionary = new File(dictionaryLocation);
+		while(!dictionary.exists() || !dictionary.isFile()){
+			System.out.println("Error: File does not exist or is not a file.");
+			System.out.print("Dictionary location: ");
+			dictionaryLocation = systemIn.next();			
+			dictionary = new File(dictionaryLocation);
 		}
 		
-		while (sc.hasNextLine()) {
-			String line = sc.nextLine();
+		systemIn.close();
+	}
+	
+	public static void populateTrie(){
+		System.out.println("\nPopulating trie...");
+		trie = new TrieNode(null);
+		words = new ArrayList<String>();
+		Scanner dictionaryScanner = null;
+		try {
+			dictionaryScanner = new Scanner(dictionary); 
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		// Scan all lines in dictionary file,
+		// insert all words that are over two characters long and less than specified word length,
+		// also add all words that are exactly specified word length long into ArrayList for processing
+		while (dictionaryScanner.hasNextLine()) {
+			String line = dictionaryScanner.nextLine();
 			if(line.matches("[a-zA-Z][a-zA-Z]+")){
-				if(line.length() < 6) // TODO UI
+				if(line.length() < wordLength) 
 					trie.insert(line.toLowerCase());
-				else if(line.length() == 6) // TODO UI
+				else if(line.length() == wordLength)
 					words.add(line.toLowerCase());
 			}			
 		}
 		
-		System.out.println(words.size());
+		System.out.println("Trie populated!");
+		dictionaryScanner.close();
 	}
 	
 	public static void findCompounds(){
+		System.out.println("\nProcessing...");
 		ArrayList<String> compounds = new ArrayList<String>();
+		int resultsCounter = 0;
 		
+		// For each word that is of specified word length,
+		// find all words that can be used as the first compound,
+		// then for each first compound word found, check if the rest of the word exists in dictionary
 		for(String word : words){
-			trie.populateCompounds(word, compounds);
-			
+			trie.populateFirstCompounds(word, compounds);
 			if(compounds.size() != 0){
 				for(String compound : compounds){
 					if(trie.lookup(word.substring(compound.length()))){
 						System.out.println(String.format("%s + %s = %s", compound, word.substring(compound.length()), word));
+						resultsCounter++;
 					}
 				}
-			}
-			
+			}			
 			compounds.clear();
 		}		
-	}
-	
-	public static void testbed(){
-		String test1 = "Ho";
-		String test2 = "omework";
-		String test3 = "Homework";
 		
-		trie.insert(test1.toLowerCase());
-		trie.insert(test2.toLowerCase());
-		trie.insert(test3.toLowerCase());
-		
-		ArrayList<String> compounds = new ArrayList<String>();
-		trie.populateCompounds(test3.toLowerCase(), compounds);
-		
-		System.out.println(trie.lookup(test3.substring(compounds.get(0).length())));
-		
-//		System.out.println(trie.lookup("homework"));
-//		System.out.println(trie.lookup("house"));
-//		System.out.println(trie.lookup("ho"));
-//		System.out.println(trie.lookup("mountain"));
-//		System.out.println(trie.lookup("mount"));
+		System.out.println(String.format("\nProcessing complete! Found %d compound words.", resultsCounter));
 	}
 
 }
--- src/TrieNode.java
+++ src/TrieNode.java
@@ -2,31 +2,66 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 
+/**
+ * @author Nick Dandakis
+ *
+ * Implements the functionality of a trie node.
+ */
 public class TrieNode {
-	private String value;
+	private String word;
 	private HashMap<Character, TrieNode> children;
 	
-	public TrieNode(String value) {
-		this.value = value;
+	/**
+	 * Constructor of TrieNode object.
+	 * 
+	 * @param word the word string that this node holds, can be null
+	 */
+	public TrieNode(String word) {
+		this.word = word;
 		this.children = new HashMap<Character, TrieNode>();
 	}
 
+	/**
+	 * Adds child TrieNode. 
+	 * 
+	 * @param key the key character
+	 * @param child the child TrieNode object
+	 */
 	public void addChild(Character key, TrieNode child){
 		children.put(key, child);
 	}
 	
-	public void removeChild(TrieNode child){
-		children.remove(child.getValue());
+	/**
+	 * Removed child TrieNode.
+	 * 
+	 * @param key the key of TrieNode to remove
+	 */
+	public void removeChild(Character key){
+		children.remove(key);
 	}
 	
-	public String getValue(){
-		return this.value;
+	/**
+	 * @return the value (word string) of this node
+	 */
+	public String getWord(){
+		return this.word;
 	}
 	
+	/**
+	 * Inserts word string into trie data structure.
+	 * 
+	 * @param word string to be inserted into trie
+	 */
 	public void insert(String word){
 		insert(word, word);
 	}
 	
+	/**
+	 * Inserts word string into trie data structure.
+	 * 
+	 * @param word string to be inserted into trie, immutable 
+	 * @param scratch initially set to word and is recursively updated (pruned) while traversing (and updating) the trie
+	 */
 	public void insert(String word, String scratch){
 		if(scratch.length() == 1){
 			addChild(scratch.charAt(0), new TrieNode(word));
@@ -37,14 +72,27 @@ public class TrieNode {
 		children.get(scratch.charAt(0)).insert(word, scratch.substring(1));
 	}
 	
+	/**
+	 * Looks for word string in trie data structure.
+	 * 
+	 * @param word string to look for in trie data structure
+	 * @return true if word found in trie, false otherwise
+	 */
 	public boolean lookup(String word){
 		return lookup(word, word);
 	}
 	
+	/**
+	 * Looks for word string in trie data structure.
+	 * 
+	 * @param word string to look for in trie data structure, immutable
+	 * @param scratch initially set to word and is recursively updated (pruned) while traversing the trie
+	 * @return true if word found in trie, false otherwise
+	 */
 	public boolean lookup(String word, String scratch){
 		if(scratch.length() == 1)
 			if(children.containsKey(scratch.charAt(0))){
-				if(children.get(scratch.charAt(0)).getValue() != null && children.get(scratch.charAt(0)).getValue().equals(word))
+				if(children.get(scratch.charAt(0)).getWord() != null && children.get(scratch.charAt(0)).getWord().equals(word))
 					return true;
 				else 
 					return false;
@@ -56,14 +104,20 @@ public class TrieNode {
 			return children.get(scratch.charAt(0)).lookup(word, scratch.substring(1));
 	}	
 	
-	public void populateCompounds(String word, ArrayList<String> compounds){
+	/**
+	 * Finds all words that can be used as first compounds for word parameter.
+	 * 
+	 * @param word string find all first compounds for
+	 * @param compounds the compounds found, clear after use
+	 */
+	public void populateFirstCompounds(String word, ArrayList<String> compounds){
 		if(word.length() == 0) 
 			return;
 		else{ 
 			if(children.containsKey(word.charAt(0))){
-				if(children.get(word.charAt(0)).getValue() != null)
-					compounds.add(children.get(word.charAt(0)).getValue());
-				children.get(word.charAt(0)).populateCompounds(word.substring(1), compounds);
+				if(children.get(word.charAt(0)).getWord() != null)
+					compounds.add(children.get(word.charAt(0)).getWord());
+				children.get(word.charAt(0)).populateFirstCompounds(word.substring(1), compounds);
 			} else
 				return;
 		}
